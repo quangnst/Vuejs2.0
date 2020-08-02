4aa3af513b4c3f6a8ce3217c3407e8089d4371d1
Nghiem Quang <quangnst@gmail.com>
2020-08-02 12:49:56 +0700

    add shopping

---
+++ vue-shoppingcart/src/pages/admin/New.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>This is Admin/New</h1>
+      <product-form 
+        @save-product="addProduct" 
+        :model="model" 
+        :manufacturers="manufacturers">
+      </product-form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import ProductFrom from '@/components/products/ProductForm.vue'
+  export default {
+    data () {
+      return {
+        model: {},
+        manufacturers: [
+          {
+            _id: 'sam',
+            name: 'Samsung',
+          },
+          {
+            _id: 'apple',
+            name: 'Apple',
+          },
+        ],
+      }
+    },
+    methods: {
+      addProduct (model) {
+        console.log('model', model)
+      }
+    },
+    components: {
+      'product-form': ProductFrom
+    }
+  }
+</script>
---
+++ vue-shoppingcart/src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/components/Home';
+import Cart from '@/components/Cart';
+
+// Admin
+import Index from '@/pages/admin/Index';
+import Products from '@/pages/admin/Products';
+import Edit from '@/pages/admin/Edit';
+import New from '@/pages/admin/New';
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/admin',
+            name: 'Admin',
+            component: Index,
+            children: [
+                {
+                    path: 'new',
+                    name: 'New',
+                    component: New
+                }, 
+                {
+                    path: '',
+                    name: 'Products',
+                    component: Products
+                },
+                {
+                    path: 'edit/:id',
+                    name: 'Edit',
+                    component: Edit
+                },
+            ]
+        },
+        {
+            path: '/cart',
+            name: 'Cart',
+            component: Cart
+        }
+    ]
+});
