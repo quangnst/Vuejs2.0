e0dd61affc53a5ceb6b84996446e8c21cfc098b7
quangnst <quangnst@gmail.com>
2020-10-22 05:52:20 +0700

    update .env

--- vue-firebase-auth/src/firebase.js
+++ vue-firebase-auth/src/firebase.js
@@ -4,14 +4,14 @@ import "firebase/firestore";
 
 // firebase init - add your own config here
 const firebaseConfig = {
-  apiKey: "AIzaSyADw-oCQbxAXqquMNhSBRse3GOwwRm4n-M",
-  authDomain: "vue-authen-33948.firebaseapp.com",
-  databaseURL: "https://vue-authen-33948.firebaseio.com",
-  projectId: "vue-authen-33948",
-  storageBucket: "vue-authen-33948.appspot.com",
-  messagingSenderId: "824858329646",
-  appId: "1:824858329646:web:dfadbf92f8bee5facf55bf",
-  measurementId: "G-2FN8XCXCHV",
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FB_DB_URL,
+  projectId: process.env.VUE_APP_FB_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_MSG_SENDER,
+  appId: process.env.VUE_APP_FB_APP_ID,
+  measurementId: process.env.VUE_APP_FB_MEASUREMENT_ID,
 };
 firebase.initializeApp(firebaseConfig);
 
@@ -22,8 +22,6 @@ const auth = firebase.auth();
 // collection references
 const usersCollection = db.collection("users");
 const postsCollection = db.collection("posts");
-const commentsCollection = db.collection("comments");
-const likesCollection = db.collection("likes");
 
 const productsCollection = db.collection("products");
 
@@ -33,7 +31,5 @@ export {
   auth,
   usersCollection,
   postsCollection,
-  commentsCollection,
-  likesCollection,
   productsCollection,
 };
